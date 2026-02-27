ca9a2fc5073fa48218b67eef53269c245043e937
ziadhatem2024 <ziadhatemdev@gmail.com>
2026-02-27 08:40:04 +0200

    feat: add Vercel Analytics integration for enhanced tracking

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 import { Navigation } from "@/components/Layout/Navigation";
 import { Footer } from "@/components/Layout/Footer";
-
+import { Analytics } from "@vercel/analytics/next"
 export default function MainLayout({
   children,
 }: Readonly<{
@@ -8,6 +8,7 @@ export default function MainLayout({
 }>) {
   return (
     <>
+      <Analytics />
       <Navigation />
       <main className="z-50 relative" data-aos="fade-up" data-aos-delay="40">
         {children}
